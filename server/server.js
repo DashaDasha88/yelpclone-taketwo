589aee994e78a75de830cd387ae9885e8492f7a4
Dasha <dasha.kotova.mail@gmail.com>
2021-05-12 20:41:31 -0400

    add restful api skeletons

--- server/server.js
+++ server/server.js
@@ -3,7 +3,35 @@ const express = require("express");
 
 const app = express(); //express instance
 
+//GET all restaurants
+app.get("/restaurants", (req, res) => {
+  res.json({
+    status: "success",
+    restaurant: "chipotle"
+  });
+});
+
+//GET a specific restaurant
+app.get("/restaurants/:id", (req, res) => {
+
+});
+
+//POST a restaurant
+app.post("/restaurants", (req, res) => {
+
+});
+
+//UPDATE a restaurant
+app.put("/restaurants/:id", (req, res) => {
+
+});
+
+//DELETE a restaurant
+app.delete("/restaurants/:id", (req, res) => {
+
+});
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log(`o hai there friend - server is litening on ${port}`);
+  console.log(`o hai - server is litening on ${port}`);
 });
