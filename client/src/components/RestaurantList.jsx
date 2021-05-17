ca93b89775be061f7d54a085f21906ef7f50cb52
Dasha <dasha.kotova.mail@gmail.com>
2021-05-16 20:07:59 -0400

    add restaurant list component

---
+++ client/src/components/RestaurantList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RestaurantList = () => {
+  return (
+    <div>
+      <h1 className="font-weight-light display-1 text-center">Restaurant List</h1>
+    </div>
+  )
+}
+
+export default RestaurantList;
