bb813298f993f57b561459d3b8de3a6109a983f3
Dasha <dasha.kotova.mail@gmail.com>
2021-05-18 19:09:51 -0400

    add restaurant context provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,23 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import RestaurantDetailsPage from "./routes/RestaurantDetailsPage";
 import UpdatePage from "./routes/UpdatePage";
+import { RestaurantsContextProvider } from "./context/RestaurantContext";
 
 const App = () => {
   return (
-    <div className="container">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/restaurants/:id/update" component={UpdatePage}/>
-          <Route exact path="/restaurants/:id" component={RestaurantDetailsPage}/>
-        </Switch>
-      </Router>
-    </div>
-  )
+    <RestaurantsContextProvider>
+      <div className="container">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/restaurants/:id/update" component={UpdatePage}/>
+            <Route exact path="/restaurants/:id" component={RestaurantDetailsPage}/>
+          </Switch>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
+  );
 
-}
+};
 
 export default App;
