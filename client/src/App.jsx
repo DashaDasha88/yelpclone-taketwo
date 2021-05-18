e1d1eb549142721034a26002e3d8a23842d010c3
Dasha <dasha.kotova.mail@gmail.com>
2021-05-18 18:51:18 -0400

    add table and format page with container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,17 @@ import RestaurantDetailsPage from "./routes/RestaurantDetailsPage";
 import UpdatePage from "./routes/UpdatePage";
 
 const App = () => {
-  return <div>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/restaurants/:id/update" component={UpdatePage}/>
-        <Route exact path="/restaurants/:id" component={RestaurantDetailsPage}/>
-      </Switch>
-    </Router>
-
-  </div>
+  return (
+    <div className="container">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/restaurants/:id/update" component={UpdatePage}/>
+          <Route exact path="/restaurants/:id" component={RestaurantDetailsPage}/>
+        </Switch>
+      </Router>
+    </div>
+  )
 
 }
 
--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -2,8 +2,29 @@ import React from "react";
 
 const RestaurantList = () => {
   return (
-    <div>
-      <h1 className="font-weight-light display-1 text-center">Restaurant List</h1>
+    <div class="list-group">
+      <table className="table table-hover table-dark">
+        <thead>
+          <tr className="bg-primary">
+            <th scopr="col">Restaurant</th>
+            <th scopr="col">Location</th>
+            <th scopr="col">Price Range</th>
+            <th scopr="col">Ratings</th>
+            <th scopr="col">Edit</th>
+            <th scopr="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Saigon Lotus</td>
+            <td>Toronto</td>
+            <td>$$</td>
+            <td>Rating</td>
+            <td><button className="btn btn-warning">Update</button></td>
+            <td><button className="btn btn-danger">Delete</button></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
